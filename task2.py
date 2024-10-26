480f3b8a88d2143c1714324e7fdf4da8ba6b55dc
Aliyan167 <aleeyan33333@gmail.com>
2024-10-26 08:49:04 -0700

    task2

--- task2.py
+++ task2.py
@@ -26,3 +26,29 @@ def bfs_shortest_path(graph, start, goal):
         # If the goal is reached, return the path
         if city == goal:
             return path
+
+        # If the city is not visited, mark it as visited
+        if city not in visited:
+            visited.add(city)
+
+            # Get neighbors of the current city
+            for neighbor in graph[city]:
+                new_path = list(path)  # Make a new copy of the current path
+                new_path.append(neighbor)  # Append the neighbor to the path
+                queue.append(new_path)
+
+    # If no path is found, return None
+    return None
+
+
+# Function to print the shortest path
+def print_shortest_path(graph, start, goal):
+    path = bfs_shortest_path(graph, start, goal)
+    if path:
+        print(f"The shortest path from {start} to {goal} is: {' -> '.join(path)}")
+    else:
+        print(f"No path found between {start} and {goal}.")
+
+
+# Find the shortest path from Islamabad to Karachi
+print_shortest_path(cities, 'Islamabad', 'Karachi')
